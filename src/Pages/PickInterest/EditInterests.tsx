79a834dced7dafe9432f29527726b865359bda1e
AdemSelmi <cliffmark672@gmail.com>
2026-04-15 11:40:23 +0100

    adding Edit Interests

--- src/Pages/PickInterest/EditInterests.tsx
+++ src/Pages/PickInterest/EditInterests.tsx
@@ -1,205 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './editinterestsstyle.css'
 const EditInterests =()=>{
-    const interests:Record<string,string[]>={
-      "Gaming": [
-        "poker",
-        "bridge",
-        "tabletop rpgs",
-        "retro gaming",
-        "speedrunning",
-        "warhammer",
-        "chess",
-        "league of legends",
-        "valorant",
-        "minecraft",
-        "poker",
-        "dungeons & dragons",
-        "esports",
-        "cosplay",
-        "board games"
-    ],
-    "Science": [
-        "stargazing",
-        "archaeology",
-        "biology",
-        "physics",
-        "astronomy",
-        "psychology"
-    ],
-    "Fitness": [
-        "parkour",
-        "pilates",
-        "crossfit",
-        "marathon running",
-        "tai chi",
-        "yoga",
-        "gym"
-    ],
-    "Sports": [
-        "mountain biking",
-        "tennis",
-        "golf",
-        "badminton",
-        "volleyball",
-        "fencing",
-        "rugby",
-        "cricket",
-        "formula 1",
-        "motocross",
-        "wrestling",
-        "football",
-        "basketball",
-        "swimming",
-        "surfing",
-        "martial arts",
-        "skateboarding"
-    ],
-    "Hobbies": [
-        "vinyl records",
-        "comic books",
-        "lego building",
-        "action figures",
-        "stamp collecting",
-        "antique restoration",
-        "cardistry",
-        "taxidermy"
-    ],
-    "Fashion": [
-        "sneaker collecting",
-        "vintage clothing"
-    ],
-    "Technology": [
-        "virtual reality",
-        "robotics",
-        "drones",
-        "3d printing",
-        "game development",
-        "data science",
-        "iot",
-        "ethical hacking",
-        "cryptography",
-        "javascript",
-        "python",
-        "artificial intelligence",
-        "cybersecurity",
-        "web development",
-        "blockchain"
-    ],
-    "Design": [
-        "ux design",
-        "interior design"
-    ],
-    "Lifestyle": [
-        "coffee brewing",
-        "wine tasting",
-        "veganism",
-        "mixology",
-        "bbq",
-        "tea ceremony",
-        "pastry art",
-        "sushi making",
-        "skincare",
-        "survivalism",
-        "minimalism",
-        "cooking",
-        "traveling",
-        "gardening",
-        "fashion",
-        "baking"
-    ],
-    "Arts": [
-        "theatre",
-        "ballet",
-        "calligraphy",
-        "origami",
-        "puppetry",
-        "photography",
-        "digital art",
-        "painting",
-        "dancing"
-    ],
-    "Entertainment": [
-        "magic tricks",
-        "true crime",
-        "documentaries",
-        "anime",
-        "movies",
-        "stand-up comedy"
-    ],
-    "Music": [
-        "opera",
-        "jazz",
-        "hip hop",
-        "k-pop",
-        "guitar",
-        "piano",
-        "singing"
-    ],
-    "Media": [
-        "blogging",
-        "vlogging",
-        "podcasts"
-    ],
-    "Wellness": [
-        "mental health",
-        "aromatherapy",
-        "journaling",
-        "meditation",
-        "meditation"
-    ],
-    "Education": [
-        "philosophy",
-        "sociology",
-        "economics",
-        "linguistics",
-        "genealogy",
-        "public speaking",
-        "history",
-        "languages"
-    ],
-    "Social": [
-        "politics",
-        "human rights",
-        "sustainability",
-        "volunteering"
-    ],
+    const [interests,setInterests]=useState([]) ;
+    useEffect(()=>{fetch(import.meta.env.VITE_SERVER_URL+"/api/interests").then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parses the body as JSON
+  })
+  .then(data =>{ setInterests(data) ; console.log(interests)})
+  .catch(error => console.error('Fetch error:', error))},[])
     
-    "Crafts": [
-        "woodworking",
-        "knitting",
-        "pottery",
-        "sewing",
-        "jewelry making",
-        "blacksmithing"
-    ],
-    "Literature": [
-        "writing",
-        "reading"
-    ],
-    "Finance": [
-        "investing",
-        "crypto"
-    ],
-    "Outdoors": [
-        "rock climbing",
-        "camping",
-        "bird watching",
-        "fishing",
-        "archery",
-        "kayaking",
-        "scuba diving",
-        "urban exploration",
-        "hiking"
-    ]
-}
+
+
+    
+
 const colors:Record<string,string>={
   'Gaming':'purple',
   'Technology':'blue',
   'Outdoors':'#184000',
   'Music':'#a73e0e'
 }
- const [picked,setPicked]= useState<string[]>(['archery','python','javacript','chess','league of legends'])           
+ const [picked,setPicked]= useState<string[]>([])           
 
   return <div className="interestPage">
     <div className='heading1'>
@@ -214,7 +36,7 @@ const colors:Record<string,string>={
         {
         interests[c].map((i)=>(<div className="intr" 
             style={{backgroundColor: picked.includes(i)?'green':'gray',border:picked.includes(i)?'1px solid white':'none'}} 
-            onClick={()=>setPicked([...picked,i])}>{i}</div>))
+            onClick={()=>setPicked([...picked,i])}>{i.name}</div>))
         
     }</div>
     </div>:<div></div>
